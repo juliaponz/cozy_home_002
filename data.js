d85be984039a45ba28c07c2cfbda28be02b85999
juliaponz <juliaponz@yahoo.com>
2025-08-14 11:58:40 +0200

    upd 6
    renew

---
+++ data.js
@@ -0,0 +1,242 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-green-red---1",
+      "name": "green-red - 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6409315121173833,
+          "pitch": -0.896104011239764,
+          "rotation": 0,
+          "target": "4-old"
+        },
+        {
+          "yaw": 3.090864412555484,
+          "pitch": -0.713118071953625,
+          "rotation": 0,
+          "target": "2-red---1"
+        },
+        {
+          "yaw": -1.0088000160821835,
+          "pitch": 0.8571203603535764,
+          "rotation": 0,
+          "target": "1-green-red---2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8314278162936155,
+          "pitch": -0.17670944942521416,
+          "title": "плита 60*120",
+          "text": "<span style=\"color: rgb(80, 0, 80); font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif; font-size: 14px;\">\"Link\": \"&lt;a href=\\\"</span>https://tdstroitel.ru/plitka/plitka-dlya-doma/plitka-dlya-pola/keramogranit-ays-byanko-bali-pei-3-60-h-120-h-0-9<span style=\"color: rgb(80, 0, 80); font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif; font-size: 14px;\">\\\"&gt;</span>Ссылка<span style=\"color: rgb(80, 0, 80); font-family: Roboto, RobotoDraft, Helvetica, Arial, sans-serif; font-size: 14px;\">&lt;/a&gt;\"</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-green-red---2",
+      "name": "green-red - 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20830882936652273,
+          "pitch": -0.3314732757893317,
+          "rotation": 0,
+          "target": "4-old"
+        },
+        {
+          "yaw": 0.35816948314689867,
+          "pitch": -0.28836023879146744,
+          "rotation": 0,
+          "target": "2-red---1"
+        },
+        {
+          "yaw": -0.0782032262485437,
+          "pitch": 0.8225327620532497,
+          "rotation": 0,
+          "target": "0-green-red---1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-red---1",
+      "name": "red - 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "yaw": -1.74839375991807,
+        "pitch": 0.18282297402516257,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5023110652425107,
+          "pitch": -0.8237811306090546,
+          "rotation": 0,
+          "target": "4-old"
+        },
+        {
+          "yaw": -3.1268963797708658,
+          "pitch": -0.6892725336446173,
+          "rotation": 0,
+          "target": "0-green-red---1"
+        },
+        {
+          "yaw": -1.0010872627506622,
+          "pitch": 0.8463901646786187,
+          "rotation": 0,
+          "target": "3-red---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-red---2-",
+      "name": "red - 2 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21748992925077282,
+          "pitch": -0.32317368841100347,
+          "rotation": 0,
+          "target": "4-old"
+        },
+        {
+          "yaw": 0.37010847826640436,
+          "pitch": -0.3150675742913691,
+          "rotation": 0,
+          "target": "0-green-red---1"
+        },
+        {
+          "yaw": -0.03800686984026491,
+          "pitch": 0.9169082241986626,
+          "rotation": 0,
+          "target": "2-red---1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-old",
+      "name": "old",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0029712855641950142,
+          "pitch": -0.6415222257354358,
+          "rotation": 0,
+          "target": "2-red---1"
+        },
+        {
+          "yaw": 0.8899445731332456,
+          "pitch": -0.5770952188029579,
+          "rotation": 0,
+          "target": "0-green-red---1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
